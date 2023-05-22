52b590c1eec3bd375c67f87e35039dfc23a676f1
Konstantin Perikov <konstantin_perikov@epam.com>
2023-05-22 23:54:41 +0100

    feat: add initial docker-compose, fixing issues with lxml

--- create_vectors.py
+++ create_vectors.py
@@ -1,11 +1,11 @@
 import json
 import time
-from tqdm import tqdm
-from flair.embeddings import OneHotEmbeddings, DocumentPoolEmbeddings
-from flair.datasets import UD_ENGLISH
-from flair.data import Sentence
 
 from elasticsearch import Elasticsearch, helpers
+from flair.data import Sentence
+from flair.datasets import UD_ENGLISH
+from flair.embeddings import DocumentPoolEmbeddings, OneHotEmbeddings
+from tqdm import tqdm
 
 # load a corpus
 corpus = UD_ENGLISH()
@@ -13,12 +13,16 @@ corpus = UD_ENGLISH()
 embeddings = OneHotEmbeddings.from_corpus(corpus, embedding_length=400)
 document_embeddings = DocumentPoolEmbeddings([embeddings])
 
-with open('data/future_corpus.json', 'r') as f:
-   future_corpus = json.load(f)
+with open("data/future_corpus.json", "r") as f:
+    future_corpus = json.load(f)
 
 client = Elasticsearch(
     "https://localhost:9200",
-    basic_auth=("elastic", "eMMWUyLnKD8i6X-B-5K",), verify_certs=False
+    basic_auth=(
+        "elastic",
+        "eMMWUyLnKD8i6X-B-5K",
+    ),
+    verify_certs=False,
 )
 
 print(client.ping())
@@ -42,11 +46,14 @@ flair_sentence = [Sentence(s) for s in tqdm(future_corpus[:5000]) if s]
 start_time = time.time()
 for i in tqdm(range(0, len(flair_sentence), 1000)):
     try:
-        document_embeddings.embed(flair_sentence[i:i+1000])
-        list_of_docs = [{'embeddings': s.embedding.detach().numpy(), "_index": "vectors"} for s in flair_sentence[i:i+1000]]
+        document_embeddings.embed(flair_sentence[i : i + 1000])
+        list_of_docs = [
+            {"embeddings": s.embedding.detach().numpy(), "_index": "vectors"}
+            for s in flair_sentence[i : i + 1000]
+        ]
         helpers.bulk(client, list_of_docs)
     except Exception as e:
         print(e)
 
 client.indices.refresh(index="vectors")
-print(time.time()- start_time)
+print(time.time() - start_time)
